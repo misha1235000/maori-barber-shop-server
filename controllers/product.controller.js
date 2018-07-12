287a6e16c6bb05d8b2b982f8aab9d983a4bed66e
Misha <misha1235000@gmail.com>
2018-07-12 11:31:12 +0300

    Refactor of appointment.controller and product.controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,30 +4,22 @@ const ProductManager = require("../managers/product.manager");
 
 let productManager = new ProductManager.ProductManager();
 
-router.get('/product', (req, res) => {
+router.get('/product', (req, res) => { // Gets all products.
     productManager.get().then((data) => {
         res.json(data);
     });
 });
 
-router.post('/product', (req, res) => {
+router.post('/product', (req, res) => { // Creates a new product.
     productManager.add(req.body.product).then((data) => {
         res.json(data);
     });
 });
 
-router.delete('/product', (req, res) => {
+router.delete('/product', (req, res) => { // Deletes a product by id.
     productManager.delete(req.body._id).then((data) => {
         res.json(data);
     });
 });
-
-/*
-router.delete('/apointment', (req, res) => {
-    userManager.deleteAllUsers().then((data) => {
-        res.json(data);
-    });
-});
-*/
 
 module.exports = router;
