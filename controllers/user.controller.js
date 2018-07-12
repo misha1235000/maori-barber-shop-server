592b2eec3fac1fcd6d5e63643aa385d0dcaeb74e
Misha <misha1235000@gmail.com>
2018-07-12 11:35:51 +0300

    Refactor of appointment.controller and user.controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const UserManager = require("../managers/user.manager");
-const AppointmentManager = require("../managers/appointment.manager");
 
 let userManager = new UserManager.UserManager();
 
-router.get('/user', (req, res) => {
+router.get('/user', (req, res) => { // Gets all users.
     userManager.get().then((data) => {
         res.json(data);
     });
 });
 
-router.post('/user', (req, res) => {
+router.post('/user', (req, res) => { // Adds a new user.
     userManager.add(req.body.user).then((data) => {
         res.json(data);
     });
 });
 
-router.delete('/user', (req, res) => {
+router.delete('/user', (req, res) => { // Deletes a user.
     userManager.delete(req.body._id).then((data) => {
         res.json(data);
     });
